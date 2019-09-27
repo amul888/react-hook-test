bd37b7156c955a49d0fa3db75faec31b67f8e4e4
Amul Chandra <AmulC@emids.com>
2019-09-27 15:33:23 +0530

    write test

--- src/Input.test.js
+++ src/Input.test.js
@@ -1,16 +1,36 @@
 import React from "react";
 import  { shallow } from "enzyme";
 
-import { findByTestAttr } from "./../test/testUtils";
+import { findByTestAttr, checkProps } from "./../test/testUtils";
 import Input from "./Input";
 
 
-const setup =()=>{
-    return shallow(<Input/>);
+const setup =(secretWord="party")=>{
+    return shallow(<Input secretWord={secretWord}/>);
 }
 
 test("Input renders without error",()=>{
     const wrapper = setup();
     const inputComponent = findByTestAttr(wrapper,"component-input");
     expect(inputComponent.length).toBe(1);
-})
+});
+
+test("does not throw warning with expected prop",()=>{
+    checkProps(Input, {secretWord:"party"})
+});
+
+describe("state controlled input filed",()=>{
+ 
+    test("state update with value of input box upon changes",()=>{
+        const mockSetCurrentGuess = jest.fn();
+        React.useState = jest.fn(()=> ["",mockSetCurrentGuess]);
+
+        const wrapper = setup();
+        const inputBox = findByTestAttr(wrapper,"input-box");
+
+        const mockEvent = { target: {value:"train"}};
+        inputBox.simulate("change",mockEvent);
+
+        expect(mockSetCurrentGuess).toHaveBeenCalledWith("train");
+    })
+});
