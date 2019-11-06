b333fd182fd97d797805695d9dca8d9147bcff9f
Amul Chandra <AmulC@emids.com>
2019-11-06 16:42:16 +0530

    react-hook all test are passing `final commit for hooks`

--- src/App.js
+++ src/App.js
@@ -1,34 +1,45 @@
-import React, { Component} from "react";
+import React, { Component } from "react";
 import hookActions from "./actions/hookActions";
 // import "./App.css";
+import Input from "./Input";
 
-function reducer(state, action){
-  switch(action.type){
+function reducer(state, action) {
+  switch (action.type) {
     case "setSecretWord":
-      return {...state, secretWord:action.payload}
-      default:
-        throw new Error(`invaild action type: ${action.type}`)
+      return { ...state, secretWord: action.payload }
+    default:
+      throw new Error(`invaild action type: ${action.type}`)
   }
 }
-function App(){
-  const [state,dispatch] = React.useReducer(
+function App() {
+  const [state, dispatch] = React.useReducer(
     reducer,
-    {secretWord: null}
+    { secretWord: null }
   )
 
-  const setSecretWord = (secretWord)=> 
-      dispatch({type:"setSecretWord", payload:secretWord})
-    
-      React.useEffect(
-        () => {hookActions.getSecretWord(setSecretWord)},
-        []
-      )
-      return(
-      <div data-test="component-app">
-        Amul
+  const setSecretWord = (secretWord) =>
+    dispatch({ type: "setSecretWord", payload: secretWord })
+
+  React.useEffect(
+    () => { hookActions.getSecretWord(setSecretWord) },
+    []
+  )
+  if(!state.secretWord){
+    return(
+      <div className="container" data-test="spinner">
+        <div className="spinner-border" role="status">
+          <span className="sr-only">Loading...</span>
         </div>
-        
-    );
+        <p>Loading secret word</p>
+      </div>
+    )
+  }
+  return (
+    <div className="container" data-test="component-app">
+      <Input secretWord={state.secretWord} />
+    </div>
+
+  );
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -17,10 +17,17 @@ const mockGetSecretWord = jest.fn();
 //     return shallow(<App/>);
 // }
 
-const setup =()=>{
+const setup =(secretWord="party")=>{
     mockGetSecretWord.mockClear();
     hookActions.getSecretWord = mockGetSecretWord;
     
+    const mockUseReducer = jest.fn()
+        .mockReturnValue([
+            {secretWord},
+            jest.fn()
+        ]);
+
+        React.useReducer = mockUseReducer;
     // use mount, beacause useEffect not call on `shallow`
     // https://github.com/airbnb/enzyme/issues/2086
     return mount(<App/>);
@@ -46,5 +53,37 @@ describe("getSecretWord calls",()=>{
 
         expect(mockGetSecretWord).not.toHaveBeenCalled();
     })
+});
+
+describe("secretWord is not null",()=>{
+    let wrapper;
+    beforeEach(()=>{
+        wrapper = setup("party")
+    });
+    test("renders app when secretWord is not null",()=>{
+        const appComponnet = findByTestAttr(wrapper,"component-app");
+        expect(appComponnet.exists()).toBe(true);
+    });
+    test("does not render spinner when secretWord is not null",()=>{
+        const spinnerComponnet = findByTestAttr(wrapper,"spinner");
+        expect(spinnerComponnet.exists()).toBe(false);
+    });
+});
+
+
+describe("secretWord is null",()=>{
+    let wrapper;
+    beforeEach(()=>{
+        wrapper = setup(null)
+    });
+    test("does not renders app when secretWord is not null",()=>{
+        const appComponnet = findByTestAttr(wrapper,"component-app");
+        expect(appComponnet.exists()).toBe(false);
+    });
+    test("render spinner when secretWord is not null",()=>{
+        const spinnerComponnet = findByTestAttr(wrapper,"spinner");
+        expect(spinnerComponnet.exists()).toBe(true);
+    });
 })
+
 
